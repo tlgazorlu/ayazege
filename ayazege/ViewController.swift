ee56049f1eadaebb5333169194862fd3a46e6801
tolga zorlu <tlgazorlu@gmail.com>
2020-05-01 17:54:43 +0300

    Initial Commit

---
+++ ayazege/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ayazege
+//
+//  Created by tolga on 1.05.2020.
+//  Copyright © 2020 tolga zorlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
